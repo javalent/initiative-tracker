408e60edb787ed63f9d722ac0159a0bab7934273
andrgram <andgram97@gmail.com>
2023-04-18 15:35:29 +0200

    Added "hidden" as creature parameter

--- src/encounter/index.ts
+++ src/encounter/index.ts
@@ -212,9 +212,14 @@ export class EncounterParser {
             ac: number | string,
             mod: number,
             xp: number,
+            hidden: boolean = false,
             friendly: boolean = false;
 
         if (typeof monster == "string") {
+            if (monster.match(/,\s+hidden/)) {
+                hidden = true;
+                monster = monster.replace(/,\s*hidden/, "");
+            }
             if (monster.match(/,\s+friend/) || monster.match(/,\s+ally/)) {
                 friendly = true;
                 monster = monster.replace(/,\s*friend/, "").replace(/,\s*ally/, "");
@@ -235,16 +240,21 @@ export class EncounterParser {
                 display = monster[0][1];
             }
 
+            hidden = monster
+                .slice(1)
+                .find((v) => v == "hidden") != undefined;
+
             friendly = monster
                 .slice(1)
                 .find((v) => v == "friend" || v == "ally") != undefined;
 
             [hp, ac, mod, xp] = monster
                 .slice(1)
-                .filter((v) => v != "friend" && v != "ally")
+                .filter((v) => v != "hidden" && v != "friend" && v != "ally")
                 .map((v) => (isNaN(Number(v)) ? null : Number(v)));
         } else if (typeof monster == "object") {
             ({ creature: name, name: display, hp, ac, mod, xp } = monster);
+            hidden = monster.hidden || false;
             friendly = monster.friend || monster.ally || false;
         }
 
@@ -259,6 +269,7 @@ export class EncounterParser {
         creature.ac = ac ?? creature.ac;
         creature.modifier = mod ?? creature.modifier;
         creature.xp = xp ?? creature.xp;
+        creature.hidden = hidden ?? creature.hidden;
         creature.friendly = friendly ?? creature.friendly;
 
         return { creature, number };
