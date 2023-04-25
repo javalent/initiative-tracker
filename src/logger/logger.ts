5996907b3666f4d71fff8ca4106c78325ddf8191
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2023-04-24 21:54:06 -0400

    fix: fixes max hp logging

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -26,8 +26,8 @@ export default class Logger {
     async setFile() {
         const file = (await this.adapter.exists(normalizePath(this.logFile)))
             ? await this.vault.getAbstractFileByPath(
-                normalizePath(this.logFile)
-            )
+                  normalizePath(this.logFile)
+              )
             : await this.vault.create(this.logFile, ``);
 
         if (file instanceof TFile) {
@@ -38,7 +38,7 @@ export default class Logger {
         return this.file;
     }
     private file: TFile;
-    constructor(public plugin: InitiativeTracker) { }
+    constructor(public plugin: InitiativeTracker) {}
     get enabled() {
         return this.plugin.data.logging;
     }
@@ -137,19 +137,38 @@ export default class Logger {
             if (message.hp) {
                 if (message.temp) {
                     perCreature.push(
-                        `${message.name
-                        } gained ${message.hp.toString()} temporary HP`
+                        `${message.name} gained ${(
+                            -1 * message.hp
+                        ).toString()} temporary HP`
                     );
+                } else if (message.max) {
+                    if (message.hp < 0) {
+                        perCreature.push(
+                            `${message.name} took ${(
+                                -1 * message.hp
+                            ).toString()} max HP damage${
+                                message.unc ? " and died" : ""
+                            }`
+                        );
+                    } else {
+                        perCreature.push(
+                            `${
+                                message.name
+                            } gained ${message.hp.toString()} max HP`
+                        );
+                    }
                 } else if (message.hp < 0) {
                     perCreature.push(
                         `${message.name} took ${(
                             -1 * message.hp
-                        ).toString()} damage${message.unc ? " and was knocked unconscious" : ""
+                        ).toString()} damage${
+                            message.unc ? " and was knocked unconscious" : ""
                         }`
                     );
                 } else if (message.hp > 0) {
                     perCreature.push(
-                        `${message.name
+                        `${
+                            message.name
                         } was healed for ${message.hp.toString()} HP`
                     );
                 }
@@ -162,7 +181,9 @@ export default class Logger {
                 }
 
                 if (message.ac_add) {
-                    perCreature.push(`${message.name} added ${message.ac} to AC`);
+                    perCreature.push(
+                        `${message.name} added ${message.ac} to AC`
+                    );
                 } else {
                     perCreature.push(`${message.name} AC set to ${message.ac}`);
                 }
