73f7ae1b8aaca996a675922746523fd8d450216b
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2023-06-28 19:42:46 -0400

    fix: fixes issue where an empty player list causes error

--- src/utils/rpg-system/dnd5e-lazygm.ts
+++ src/utils/rpg-system/dnd5e-lazygm.ts
@@ -1,6 +1,11 @@
 import type InitiativeTracker from "src/main";
 import type { DifficultyLevel, GenericCreature, DifficultyThreshold } from ".";
-import { DEFAULT_UNDEFINED, convertFraction, crToString, getFromCreatureOrBestiary } from "src/utils";
+import {
+    DEFAULT_UNDEFINED,
+    convertFraction,
+    crToString,
+    getFromCreatureOrBestiary
+} from "src/utils";
 import { RpgSystem } from "./rpgSystem";
 import { Dnd5eRpgSystem } from "./dnd5e";
 
@@ -8,67 +13,77 @@ export class Dnd5eLazyGmRpgSystem extends RpgSystem {
     plugin: InitiativeTracker;
     dnd5eRpgSystem: Dnd5eRpgSystem;
 
-  constructor(plugin: InitiativeTracker) {
-      super();
-      this.plugin = plugin;
-      this.valueUnit = "CR";
-      this.displayName = "DnD 5e Lazy GM";
-      this.dnd5eRpgSystem = new Dnd5eRpgSystem(plugin);
-  }
+    constructor(plugin: InitiativeTracker) {
+        super();
+        this.plugin = plugin;
+        this.valueUnit = "CR";
+        this.displayName = "DnD 5e Lazy GM";
+        this.dnd5eRpgSystem = new Dnd5eRpgSystem(plugin);
+    }
 
-  getCreatureDifficulty(creature: GenericCreature, _?: number[]): number {
-      return convertFraction(
-          getFromCreatureOrBestiary(this.plugin, creature, c => c?.cr ?? 0));
-  }
+    getCreatureDifficulty(creature: GenericCreature, _?: number[]): number {
+        return convertFraction(
+            getFromCreatureOrBestiary(this.plugin, creature, (c) => c?.cr ?? 0)
+        );
+    }
 
-  getAdditionalCreatureDifficultyStats(
-    creature: GenericCreature,
-    _?: number[]
-  ): string[] {
-      const xp = this.dnd5eRpgSystem.getCreatureDifficulty(creature);
-      return [this.dnd5eRpgSystem.formatDifficultyValue(xp, true)];
-  }
+    getAdditionalCreatureDifficultyStats(
+        creature: GenericCreature,
+        _?: number[]
+    ): string[] {
+        const xp = this.dnd5eRpgSystem.getCreatureDifficulty(creature);
+        return [this.dnd5eRpgSystem.formatDifficultyValue(xp, true)];
+    }
 
-  getDifficultyThresholds(playerLevels: number[]): DifficultyThreshold[] {
-      const totalLevels = playerLevels.reduce((acc, lv) => acc + lv);
-      const avgLevel = playerLevels.length > 0
-          ? totalLevels / playerLevels.length : 0;
-      return [{
-          displayName: "Deadly",
-          minValue: totalLevels / (avgLevel > 4 ? 2 : 4)
-      }];
-  }
+    getDifficultyThresholds(playerLevels: number[]): DifficultyThreshold[] {
+        const totalLevels = playerLevels.reduce((acc, lv) => acc + lv, 0);
+        const avgLevel =
+            playerLevels.length > 0 ? totalLevels / playerLevels.length : 0;
+        return [
+            {
+                displayName: "Deadly",
+                minValue: totalLevels / (avgLevel > 4 ? 2 : 4)
+            }
+        ];
+    }
 
-  getEncounterDifficulty(
-    creatures: Map<GenericCreature, number>,
-    playerLevels: number[]
-  ): DifficultyLevel {
-    const crSum = [...creatures].reduce((acc, [creature, count]) =>
-        acc + this.getCreatureDifficulty(creature) * count, 0);
-    const deadlyThreshold = this.getDifficultyThresholds(playerLevels)
-        .first()?.minValue ?? 0;
-    const displayName = crSum > deadlyThreshold ? "Deadly" : "Not Deadly";
-    const xp = [...creatures].reduce((acc, [creature, count]) =>
-        acc + this.dnd5eRpgSystem.getCreatureDifficulty(creature) * count, 0);
+    getEncounterDifficulty(
+        creatures: Map<GenericCreature, number>,
+        playerLevels: number[]
+    ): DifficultyLevel {
+        const crSum = [...creatures].reduce(
+            (acc, [creature, count]) =>
+                acc + this.getCreatureDifficulty(creature) * count,
+            0
+        );
+        const deadlyThreshold =
+            this.getDifficultyThresholds(playerLevels).first()?.minValue ?? 0;
+        const displayName = crSum > deadlyThreshold ? "Deadly" : "Not Deadly";
+        const xp = [...creatures].reduce(
+            (acc, [creature, count]) =>
+                acc +
+                this.dnd5eRpgSystem.getCreatureDifficulty(creature) * count,
+            0
+        );
 
-    const summary = `Encounter is ${displayName}
+        const summary = `Encounter is ${displayName}
 Total XP: ${xp}
 Total CR: ${crSum}
-Total levels: ${playerLevels.reduce((acc, lv) => acc + lv)}
+Total levels: ${playerLevels.reduce((acc, lv) => acc + lv, 0)}
 Deadly Threshold: ${deadlyThreshold}`;
 
-    return {
-        displayName,
-        summary,
-        cssClass: displayName == "Deadly" ? "deadly" : "easy",
-        value: crSum,
-        title: "Total CR",
-        intermediateValues: [{label: "Total XP", value: xp}]
-    };
-  }
+        return {
+            displayName,
+            summary,
+            cssClass: displayName == "Deadly" ? "deadly" : "easy",
+            value: crSum,
+            title: "Total CR",
+            intermediateValues: [{ label: "Total XP", value: xp }]
+        };
+    }
 
-  formatDifficultyValue(value: number, withUnits?: boolean): string {
-    if (!value) return DEFAULT_UNDEFINED;
-    return crToString(value) + (withUnits ? " CR" : "");
-  }
+    formatDifficultyValue(value: number, withUnits?: boolean): string {
+        if (!value) return DEFAULT_UNDEFINED;
+        return crToString(value) + (withUnits ? " CR" : "");
+    }
 }
