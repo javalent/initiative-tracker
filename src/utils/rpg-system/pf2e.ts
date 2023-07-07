4bab50daab1b455b73c4229a1be583b8af2085b5
lslavkov <slavkovic.lazar96@gmail.com>
2023-07-07 09:43:05 +0200

    add & refactor: added encounter difficulty and some formatting

--- src/utils/rpg-system/pf2e.ts
+++ src/utils/rpg-system/pf2e.ts
@@ -22,29 +22,28 @@ import InitiativeTracker from '../../main'
 //   [7, 160],
 // ]);
 
-
-const XP_CREATURE_DIFFERENCES : Record<string,number> = {
-  "-4":10,
-  "-3":15,
-  "-2":20,
-  "-1":30,
-  "0":40,
-  "1":60,
-  "2":80,
-  "3":120,
-  "4":160
+const XP_CREATURE_DIFFERENCES: Record<string, number> = {
+	'-4': 10,
+	'-3': 15,
+	'-2': 20,
+	'-1': 30,
+	'0': 40,
+	'1': 60,
+	'2': 80,
+	'3': 120,
+	'4': 160,
 }
 
-const XP_SIMPLE_HAZARD_DIFFERENCES : Record<string, number> ={
-  "-4": 2,
-  "-3": 3,
-  "-2": 4,
-  "-1": 6,
-  "0": 8,
-  "1": 12,
-  "2": 16,
-  "3": 24,
-  "4":32
+const XP_SIMPLE_HAZARD_DIFFERENCES: Record<string, number> = {
+	'-4': 2,
+	'-3': 3,
+	'-2': 4,
+	'-1': 6,
+	'0': 8,
+	'1': 12,
+	'2': 16,
+	'3': 24,
+	'4': 32,
 }
 
 export class PF2eRpgSystem extends RpgSystem {
@@ -54,7 +53,10 @@ export class PF2eRpgSystem extends RpgSystem {
 		this.plugin = plugin
 		this.displayName = 'Pathfinder 2e'
 	}
-	getCreatureDifficulty(creature: GenericCreature, playerLevels?: number[]): number {
+	getCreatureDifficulty(
+		creature: GenericCreature,
+		playerLevels?: number[]
+	): number {
 		const lvl = getFromCreatureOrBestiary(
 			this.plugin,
 			creature,
@@ -62,8 +64,56 @@ export class PF2eRpgSystem extends RpgSystem {
 		)
 		if (lvl) return lvl ?? 0
 
-    const creature_differences = String(-(getFromCreatureOrBestiary(this.plugin, playerLevels, (p) => p?.level) - lvl))
+		const creature_differences = String(
+			-(
+				getFromCreatureOrBestiary(this.plugin, playerLevels, (p) => p?.level) -
+				lvl
+			)
+		)
+
+		return XP_CREATURE_DIFFERENCES[creature_differences] ?? 0
+	}
+
+	getDifficultyThresholds(playerLevels: number[]): DifficultyThreshold[] {
+		const budget = playerLevels.length * 20
+		const encounterBudget: Record<string, number> = {
+			trivial: Math.floor(budget * 0.5),
+			low: Math.floor(budget * 0.75),
+			moderate: budget,
+			severe: Math.floor(budget * 1.5),
+			extreme: Math.floor(budget * 2),
+    }
+    return Object.entries(encounterBudget).map(([name, value]) => ({
+      displayName: (name.charAt(0).toUpperCase()),
+      minValue: value
+    })).sort((a,b)=> a.minValue - b.minValue)
+	}
+
+	getEncounterDifficulty(
+		creatures: Map<GenericCreature, number>,
+		playerLevels: number[]
+	): DifficultyLevel {
+		const creatureXp = [...creatures].reduce(
+			(acc, [creature, count]) =>
+				acc + this.getCreatureDifficulty(creature) + count,
+			0
+    )
     
-    return XP_CREATURE_DIFFERENCES[creature_differences] ?? 0
+    const thresholds = this.getDifficultyThresholds(playerLevels)
+    const displayName = thresholds.reverse().find(threshold => threshold.minValue)?.displayName ?? "Trivial"
+    const thresholdSummary = thresholds.map((threshold) => `${threshold.displayName}: ${threshold.minValue}`).join('\n')
+    const summary = `Encounter is ${displayName}
+    Total XP: ${creatureXp}
+    Threshold
+    ${thresholdSummary}`;
+
+    return {
+      displayName,
+      summary,
+      cssClass: displayName.toLowerCase(),
+      value: 0,
+      title: "Adjusted XP",
+      intermediateValues: [{label: "Total XP", value: creatureXp}],
+    };
 	}
 }
