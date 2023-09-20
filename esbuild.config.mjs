3ffbf012a70d274ef99f27529c0f04d6955cd158
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2023-09-20 08:46:22 -0400

    chore: update esbuild

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -17,29 +17,41 @@ const prod = process.argv[2] === "production";
 
 const dir = prod ? "./" : process.env.OUTDIR;
 
-esbuild
-    .build({
-        banner: {
-            js: banner
-        },
-        entryPoints: ["src/main.ts", "src/styles.css"],
-        bundle: true,
-        external: ["obsidian", "electron", ...builtins],
-        format: "cjs",
-        watch: !prod,
-        target: "es2020",
-        logLevel: "info",
-        sourcemap: prod ? false : "inline",
-        treeShaking: true,
-        plugins: [
-            sveltePlugin({
-                compilerOptions: { css: true },
-                preprocess: sveltePreprocess(),
-                filterWarnings: (warning) => {
-                    return warning.code != "a11y-click-events-have-key-events";
-                }
-            })
-        ],
-        outdir: dir
-    })
-    .catch(() => process.exit(1));
+const parameters = {
+    banner: {
+        js: banner
+    },
+    entryPoints: ["src/main.ts", "src/styles.css"],
+    bundle: true,
+    external: ["obsidian", "electron", ...builtins],
+    format: "cjs",
+    logLevel: "info",
+    target: "es2020",
+    treeShaking: true,
+    sourcemap: prod ? false : "inline",
+    minify: prod,
+    plugins: [
+        sveltePlugin({
+            compilerOptions: { css: true },
+            preprocess: sveltePreprocess(),
+            filterWarnings: (warning) => {
+                return warning.code != "a11y-click-events-have-key-events";
+            }
+        })
+    ],
+    outdir: dir
+};
+
+if (prod) {
+    await esbuild.build(parameters).catch((x) => {
+        if (x.errors) {
+            console.error(x.errors);
+        } else {
+            console.error(x);
+        }
+        process.exit(1);
+    });
+} else {
+    let ctx = await esbuild.context(parameters);
+    await ctx.watch();
+}
