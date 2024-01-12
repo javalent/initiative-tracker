d8639ec667a8ad44c1a025b96e4b2d46a4bcde82
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2024-01-11 20:06:12 -0500

    fix: fix error caused by no player level being set

--- src/utils/rpg-system/dnd5e.ts
+++ src/utils/rpg-system/dnd5e.ts
@@ -3,16 +3,18 @@ import { crToString, getFromCreatureOrBestiary } from "..";
 import type InitiativeTracker from "src/main";
 import type { DifficultyLevel, GenericCreature, DifficultyThreshold } from ".";
 
-const XP_THRESHOLDS_PER_LEVEL: { [level: number]: { [threshold: string]: number} } = {
-    1:  { daily: 300, easy: 25, medium: 50, hard: 75, deadly: 100 },
-    2:  { daily: 600, easy: 50, medium: 100, hard: 150, deadly: 200 },
-    3:  { daily: 1200, easy: 75, medium: 150, hard: 225, deadly: 400 },
-    4:  { daily: 1700, easy: 125, medium: 250, hard: 375, deadly: 500 },
-    5:  { daily: 3500, easy: 250, medium: 500, hard: 750, deadly: 1100 },
-    6:  { daily: 4000, easy: 300, medium: 600, hard: 900, deadly: 1400 },
-    7:  { daily: 5000, easy: 350, medium: 750, hard: 1100, deadly: 1700 },
-    8:  { daily: 6000, easy: 450, medium: 900, hard: 1400, deadly: 2100 },
-    9:  { daily: 7500, easy: 550, medium: 1100, hard: 1600, deadly: 2400 },
+const XP_THRESHOLDS_PER_LEVEL: {
+    [level: number]: { [threshold: string]: number };
+} = {
+    1: { daily: 300, easy: 25, medium: 50, hard: 75, deadly: 100 },
+    2: { daily: 600, easy: 50, medium: 100, hard: 150, deadly: 200 },
+    3: { daily: 1200, easy: 75, medium: 150, hard: 225, deadly: 400 },
+    4: { daily: 1700, easy: 125, medium: 250, hard: 375, deadly: 500 },
+    5: { daily: 3500, easy: 250, medium: 500, hard: 750, deadly: 1100 },
+    6: { daily: 4000, easy: 300, medium: 600, hard: 900, deadly: 1400 },
+    7: { daily: 5000, easy: 350, medium: 750, hard: 1100, deadly: 1700 },
+    8: { daily: 6000, easy: 450, medium: 900, hard: 1400, deadly: 2100 },
+    9: { daily: 7500, easy: 550, medium: 1100, hard: 1600, deadly: 2400 },
     10: { daily: 9000, easy: 600, medium: 1200, hard: 1900, deadly: 2800 },
     11: { daily: 10500, easy: 800, medium: 1600, hard: 2400, deadly: 3600 },
     12: { daily: 11500, easy: 1000, medium: 2000, hard: 3000, deadly: 4500 },
@@ -67,103 +69,137 @@ const XP_PER_CR: Record<string, number> = {
 };
 
 export class Dnd5eRpgSystem extends RpgSystem {
-  plugin: InitiativeTracker;
+    plugin: InitiativeTracker;
 
-  override systemDifficulties: [string, string, ...string[]] = [
-		"Easy",
-		"Medium",
-		"Hard",
-		"Deadly"
-	]
+    override systemDifficulties: [string, string, ...string[]] = [
+        "Easy",
+        "Medium",
+        "Hard",
+        "Deadly"
+    ];
 
-  constructor(plugin: InitiativeTracker) {
-    super();
-    this.plugin = plugin;
-    this.displayName = "DnD 5e";
-  }
+    constructor(plugin: InitiativeTracker) {
+        super();
+        this.plugin = plugin;
+        this.displayName = "DnD 5e";
+    }
 
-  getCreatureDifficulty(creature: GenericCreature, _?: number[]): number {
-    const xp = getFromCreatureOrBestiary(this.plugin, creature, c => c?.xp ?? 0);
-    if (xp) return xp;
-    const cr = getFromCreatureOrBestiary(this.plugin, creature, c => c?.cr ?? "0");
-    return XP_PER_CR[cr] ?? 0;
-  }
+    getCreatureDifficulty(creature: GenericCreature, _?: number[]): number {
+        const xp = getFromCreatureOrBestiary(
+            this.plugin,
+            creature,
+            (c) => c?.xp ?? 0
+        );
+        if (xp) return xp;
+        const cr = getFromCreatureOrBestiary(
+            this.plugin,
+            creature,
+            (c) => c?.cr ?? "0"
+        );
+        return XP_PER_CR[cr] ?? 0;
+    }
 
-  getAdditionalCreatureDifficultyStats(
-      creature: GenericCreature,
-      _?: number[]
-  ): string[] {
-      const cr = getFromCreatureOrBestiary(
-          this.plugin, creature, c => c?.cr ?? 0);
-      return [`${crToString(cr)} CR`];
-  }
+    getAdditionalCreatureDifficultyStats(
+        creature: GenericCreature,
+        _?: number[]
+    ): string[] {
+        const cr = getFromCreatureOrBestiary(
+            this.plugin,
+            creature,
+            (c) => c?.cr ?? 0
+        );
+        return [`${crToString(cr)} CR`];
+    }
 
-  getEncounterDifficulty(
-    creatures: Map<GenericCreature, number>,
-    playerLevels: number[]
-  ) : DifficultyLevel {
-    const creatureXp = [...creatures].reduce(
-      (acc, [creature, count]) => acc + this.getCreatureDifficulty(creature) * count, 0);
-    const creatureCount = [...creatures.values()].reduce((acc, cur) => acc + cur, 0);
-    const mult = this.#getXpMult(creatureCount, playerLevels.length);
-    const adjustedXp = (playerLevels.length == 0 || creatureCount == 0)
-      ? 0 : creatureXp * mult;
+    getEncounterDifficulty(
+        creatures: Map<GenericCreature, number>,
+        playerLevels: number[]
+    ): DifficultyLevel {
+        const creatureXp = [...creatures].reduce(
+            (acc, [creature, count]) =>
+                acc + this.getCreatureDifficulty(creature) * count,
+            0
+        );
+        const creatureCount = [...creatures.values()].reduce(
+            (acc, cur) => acc + cur,
+            0
+        );
+        const mult = this.#getXpMult(creatureCount, playerLevels.length);
+        const adjustedXp =
+            playerLevels.length == 0 || creatureCount == 0
+                ? 0
+                : creatureXp * mult;
 
-    const thresholds = this.getDifficultyThresholds(playerLevels);
-    const displayName = thresholds
-      .reverse()  // Should now be in descending order
-      .find(threshold => adjustedXp >= threshold.minValue)?.displayName
-      ?? "Trivial";
+        const thresholds = this.getDifficultyThresholds(playerLevels);
+        const displayName =
+            thresholds
+                .reverse() // Should now be in descending order
+                .find((threshold) => adjustedXp >= threshold.minValue)
+                ?.displayName ?? "Trivial";
 
-    const thresholdSummary = thresholds
-      .map(threshold => `${threshold.displayName}: ${threshold.minValue}`)
-      .join("\n");
+        const thresholdSummary = thresholds
+            .map(
+                (threshold) => `${threshold.displayName}: ${threshold.minValue}`
+            )
+            .join("\n");
 
-    const summary = `Encounter is ${displayName}
+        const summary = `Encounter is ${displayName}
 Total XP: ${creatureXp}
 Adjusted XP: ${adjustedXp} (x${mult})
 
 Threshold
 ${thresholdSummary}`;
 
-    return {
-      displayName,
-      summary,
-      cssClass: displayName.toLowerCase(),
-      value: adjustedXp,
-      title: "Adjusted XP",
-      intermediateValues: [{label: "Total XP", value: creatureXp}],
-    };
-  }
+        return {
+            displayName,
+            summary,
+            cssClass: displayName.toLowerCase(),
+            value: adjustedXp,
+            title: "Adjusted XP",
+            intermediateValues: [{ label: "Total XP", value: creatureXp }]
+        };
+    }
 
-  getDifficultyThresholds(playerLevels: number[]): DifficultyThreshold[] {
-    const budget: Record<string, number> = {
-      easy: 0,
-      medium: 0,
-      hard: 0,
-      deadly: 0,
-    };
-    const clampedLevels = playerLevels.map(lv => Math.max(1, Math.min(lv, 20)));
-    Object.keys(budget).forEach(key => {
-      budget[key] += clampedLevels.reduce(
-        (acc, lv) => acc + XP_THRESHOLDS_PER_LEVEL[lv][key], 0);
-    });
-    return Object.entries(budget)
-      .map(([name, value]) => ({
-        displayName: (name.charAt(0).toUpperCase() + name.slice(1)),
-        minValue: value
-      }))
-      .sort((a, b) => a.minValue - b.minValue);
-  }
+    getDifficultyThresholds(playerLevels: number[]): DifficultyThreshold[] {
+        const budget: Record<string, number> = {
+            easy: 0,
+            medium: 0,
+            hard: 0,
+            deadly: 0
+        };
+        const clampedLevels = playerLevels
+            .filter((lvl) => lvl && lvl > 0)
+            .map((lv) => Math.max(1, Math.min(lv, 20)));
+        Object.keys(budget).forEach((key) => {
+            budget[key] += clampedLevels.reduce(
+                (acc, lv) => acc + XP_THRESHOLDS_PER_LEVEL[lv]?.[key] ?? 0,
+                0
+            );
+        });
+        return Object.entries(budget)
+            .map(([name, value]) => ({
+                displayName: name.charAt(0).toUpperCase() + name.slice(1),
+                minValue: value
+            }))
+            .sort((a, b) => a.minValue - b.minValue);
+    }
 
-  getAdditionalDifficultyBudgets(playerLevels: number[]): DifficultyThreshold[] {
-    return [{
-      displayName: "Daily Budget",
-      minValue: playerLevels.reduce(
-        (acc, lv) => acc + XP_THRESHOLDS_PER_LEVEL[Math.max(1, Math.min(lv, 20))].daily,
-        0)
-    }];
-  }
+    getAdditionalDifficultyBudgets(
+        playerLevels: number[]
+    ): DifficultyThreshold[] {
+        return [
+            {
+                displayName: "Daily Budget",
+                minValue: playerLevels.reduce(
+                    (acc, lv) =>
+                        acc +
+                        XP_THRESHOLDS_PER_LEVEL[Math.max(1, Math.min(lv, 20))]
+                            .daily,
+                    0
+                )
+            }
+        ];
+    }
 
     #getXpMult(creatureCount: number, playerCount: number) {
         const mults = [0.5, 1, 1.5, 2, 2.5, 3, 4, 5];
